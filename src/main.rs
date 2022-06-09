6e2bea1abf80d558b40fcfb6856af843f8a8bdc5
Micah Pixton <micah@pixtons.com>
2022-06-08 22:56:59 -0600

    FEAT: start a game in main

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,74 @@
+#![allow(dead_code)]
+
 use std::io;
 
 mod card;
 mod deck;
 mod game;
 mod player;
+mod rank;
+mod suit;
+mod trick;
 
-use crate::deck::Deck;
-use crate::player::{AIPlayer, HumanPlayer};
+use crate::card::Card;
+use crate::deck::{Deck, DeckType};
+use crate::game::MormonBridgeGame;
+use crate::player::HumanPlayer;
+use crate::trick::Trick;
 
 fn main() {
-    let mut deck = Deck::new(52);
+    // let mut deck = Deck::new(DeckType::Full, true);
 
-    println!("Deck created with {} cards", deck.total_cards());
+    // let players: [String; 4] = [
+    //     String::from("Mickey Mouse"),
+    //     String::from("Minnie Mouse"),
+    //     String::from("Donald Duck"),
+    //     String::from("Daffy Duck"),
+    // ];
 
-    deck.shuffle(Some(7));
+    // let mut cards_played: Vec<(Card, &String)> = Vec::with_capacity(4);
 
-    // deck.debug_deck();
+    // for player in players.iter() {
+    //     cards_played.push((deck.deal(), player));
+    // }
 
-    let mut player_name = String::new();
+    // let mut trick = Trick::new(cards_played, deck.deal());
 
-    println!("What is your name?");
-    while player_name.trim().is_empty() {
-        io::stdin().read_line(&mut player_name).unwrap();
-        if player_name.trim().is_empty() {
-            println!("No name recieved, please try again.");
-            println!("What is your name?");
-        }
+    // trick.show_results();
+
+    println!("Welcome to Mormon Bridge!");
+
+    // io::stdin()
+    //     .read_line(&mut guess)
+    //     .expect("Failed to read line");
+
+    // let guess: u32 = match guess.trim().parse() {
+    //     Ok(num) => num,
+    //     Err(_) => continue,
+    // };
+
+    let num_players: usize;
+
+    loop {
+        let mut input = String::new();
+        println!("How many computer opponents would you like to play with?");
+        println!("Choose a number between 1 and 6.");
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                match input.trim().parse() {
+                    Ok(num) => match num {
+                        1..=6 => {
+                            num_players = num;
+                            break;
+                        }
+                        _ => println!("{} is not between 1 and 6", num),
+                    },
+                    Err(_) => println!("The value you provided is not a number!"),
+                };
+            }
+            Err(_) => println!("Error attempting to read input."),
+        };
     }
 
-    let player_name = player_name.trim().to_string();
-
-    let mut player = HumanPlayer::new(player_name);
-
-    let mut opponent = AIPlayer::new(String::from("Minnie Mouse"));
-
-    player.add_card_to_hand(deck.deal());
-    opponent.add_card_to_hand(deck.deal());
-
-    println!("Hello {}", player);
-    println!("Player hand");
-    player.print_hand();
-    println!("Your opponent is {}", opponent);
-    println!("Their hand is");
-    opponent.print_hand();
-
-    println!("You played the {}", player.play_card().unwrap());
+    MormonBridgeGame::new(num_players);
 }
